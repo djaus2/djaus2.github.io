28995308f36dfc11cf5288fca1e48cf9036c4827
djaus2 <davidjones@sportronics.com.au>
2018-08-23 16:24:22 +1000

    Initial commit

--- bin/http2md.bat
+++ bin/http2md.bat
@@ -14,10 +14,10 @@ IF %2.==. GOTO No1
 
 @echo Going
 
-curl %1 > %2.html
+curl %1 > temp.html
 
 
-if exist %2.html (
+if exist temp.html (
 GOTO Do3
 ) 
 
@@ -26,18 +26,26 @@ GOTO End1
 
 
 :Do3
-ECHO Converting %2.html to %2.md 
-pandoc %2.html -o %2.md  %RefLink%
+ECHO Converting temp.html to %2.md 
+pandoc temp.html -o temp.md  %RefLink%
+echo --- > %2.md
+echo layout: default >> %2.md
+echo title: "%2" >> %2.md
+echo --- >> %2.md
+echo.  >> %2.md
+type temp.md >> %2.md
+del /q temp.md
+del /q temp.html
 GOTO End1
 
 :No1
-ECHO .
+ECHO.
 ECHO HTML to Markdown
 ECHO -----------------
 ECHO Must be HTML document
-ECHO .
+ECHO.
 ECHO No param 1: Need HTML document filename or basename
-ECHO .
+ECHO.
 ECHO Uses Pandoc
 ECHO Raw option if there is a second parameter
 
